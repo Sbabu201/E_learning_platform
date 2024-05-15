eb5f6b5633fb1307a27c545d23bbe08ce0df92ae
Sbabu201 <152692430+Sbabu201@users.noreply.github.com>
2024-05-15 15:19:31 +0530

    changed the db

--- server/Database.js
+++ server/Database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const MongoDb = async () => {
     try {
-        await mongoose.connect(process.env.DB)
+        await mongoose.connect("mongodb+srv://e-learning:e-learning@cluster0.mwwvbid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("connected to the database")
     } catch (error) {
         console.log('error', error)
